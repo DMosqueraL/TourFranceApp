10cb57d1e95249f6601b06455278d637bee7b16a
DMosqueral <dml2410@gmail.com>
2022-07-14 07:22:46 -0500

    [FEAT] Creación Clases Cyclist y Team
    Se crean las clases de las colecciones Cyclist y Team con sus respectivos atributos

--- src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/collection/Cyclist.java
+++ src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/collection/Cyclist.java
@@ -1,4 +1,23 @@
 package co.com.sofka.zonatalentos.tourfranceapp.cyclist.collection;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "cyclists")
 public class Cyclist {
+    @Id
+    private String idCyclist;
+    private String name;
+    private String numCompetitor;
+    private String nameTeam;
+    private String nacionality;
+
 }
--- src/main/java/co/com/sofka/zonatalentos/tourfranceapp/team/collection/Team.java
+++ src/main/java/co/com/sofka/zonatalentos/tourfranceapp/team/collection/Team.java
@@ -1,4 +1,23 @@
 package co.com.sofka.zonatalentos.tourfranceapp.team.collection;
 
+import co.com.sofka.zonatalentos.tourfranceapp.cyclist.collection.Cyclist;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "teams")
 public class Team {
+    @Id
+    private String idTeam;
+    private String nameTeam;
+    private String codeTeam;
+    private String partnerCountry;
+    private List<Cyclist> cyclistsTeam;
 }
