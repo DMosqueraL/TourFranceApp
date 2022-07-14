c1f7c1fb676cfde45eeac2ac0baf2fc375ce0710
DMosqueral <dml2410@gmail.com>
2022-07-14 07:42:51 -0500

    [FEAT] Creación CyclistDTO y TeamDTO
    Se crean las clases DTO para Cyclist y Team con sus respectivos atributos

--- src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/dto/CyclistDTO.java
+++ src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/dto/CyclistDTO.java
@@ -1,4 +1,22 @@
 package co.com.sofka.zonatalentos.tourfranceapp.cyclist.dto;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class CyclistDTO {
+    private String idCyclist;
+    private String name;
+    private String numCompetitor;
+    private String nameTeam;
+    private String nacionality;
+
+    public CyclistDTO(String idCyclist, String name, String numCompetitor, String nameTeam, String nacionality) {
+        this.idCyclist = idCyclist;
+        this.name = name;
+        this.numCompetitor = numCompetitor;
+        this.nameTeam = nameTeam;
+        this.nacionality = nacionality;
+    }
 }
--- src/main/java/co/com/sofka/zonatalentos/tourfranceapp/team/dto/TeamDTO.java
+++ src/main/java/co/com/sofka/zonatalentos/tourfranceapp/team/dto/TeamDTO.java
@@ -1,4 +1,25 @@
 package co.com.sofka.zonatalentos.tourfranceapp.team.dto;
 
+import co.com.sofka.zonatalentos.tourfranceapp.cyclist.collection.Cyclist;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
 public class TeamDTO {
+    private String idTeam;
+    private String nameTeam;
+    private String codeTeam;
+    private String partnerCountry;
+    private List<Cyclist> cyclistsTeam;
+
+    public TeamDTO(String idTeam, String nameTeam, String codeTeam, String partnerCountry, List<Cyclist> cyclistsTeam) {
+        this.idTeam = idTeam;
+        this.nameTeam = nameTeam;
+        this.codeTeam = codeTeam;
+        this.partnerCountry = partnerCountry;
+        this.cyclistsTeam = cyclistsTeam;
+    }
 }
