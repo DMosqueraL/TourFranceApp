8537a47c29795b5ffc95cc9e63556c453ada19f8
DMosqueral <dml2410@gmail.com>
2022-07-14 17:56:16 -0500

    [FEAT] Router: ListCyclistsByNameTeamRouter
    Se implementó la ruta para obtener la lista de ciclistas por el nombre del equipo

---
+++ src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/routers/ListCyclistsByNameTeamRouter.java
@@ -0,0 +1,30 @@
+package co.com.sofka.zonatalentos.tourfranceapp.cyclist.routers;
+
+import co.com.sofka.zonatalentos.tourfranceapp.cyclist.dto.CyclistDTO;
+import co.com.sofka.zonatalentos.tourfranceapp.cyclist.usecases.ListCyclistsByNameTeamUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class ListCyclistsByNameTeamRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> ListCyclistsByNameTeam(ListCyclistsByNameTeamUseCase listCyclistsByNameTeamUseCase){
+        return route(
+                GET("/cyclist/list/{nameTeam}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(listCyclistsByNameTeamUseCase
+                                .apply(request.pathVariable("nameTeam")),
+                                CyclistDTO.class))
+        );
+    }
+}
