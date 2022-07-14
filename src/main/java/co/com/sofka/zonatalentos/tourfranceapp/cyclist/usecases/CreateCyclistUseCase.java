ba19e816ca431e2e734e1d7911bb8b3dabcab43f
DMosqueral <dml2410@gmail.com>
2022-07-14 10:56:43 -0500

    [FEAT] Craso de Uso: CreateCyclistUseCase
    Se cimpleemntó  caso de uso para la creación de un ciclista

--- src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/usecases/CreateCyclistUseCase.java
+++ src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/usecases/CreateCyclistUseCase.java
@@ -1,4 +1,28 @@
 package co.com.sofka.zonatalentos.tourfranceapp.cyclist.usecases;
 
-public class CreateCyclistUseCase {
+import co.com.sofka.zonatalentos.tourfranceapp.cyclist.collection.Cyclist;
+import co.com.sofka.zonatalentos.tourfranceapp.cyclist.dto.CyclistDTO;
+import co.com.sofka.zonatalentos.tourfranceapp.cyclist.mapper.CyclistMappers;
+import co.com.sofka.zonatalentos.tourfranceapp.cyclist.repository.CyclistRepository;
+import co.com.sofka.zonatalentos.tourfranceapp.cyclist.usecases.interfaces.SaveCyclist;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+import reactor.core.publisher.Mono;
+
+@Service
+@Validated
+public class CreateCyclistUseCase implements SaveCyclist {
+    private final CyclistRepository cyclistRepository;
+    private final CyclistMappers mappers;
+
+    public CreateCyclistUseCase(CyclistRepository cyclistRepository, CyclistMappers mappers) {
+        this.cyclistRepository = cyclistRepository;
+        this.mappers = mappers;
+    }
+    @Override
+    public Mono<String> apply(CyclistDTO cyclistDTO) {
+        return cyclistRepository
+                .save(mappers.mapCyclistDTOToCyclist().apply(cyclistDTO))
+                .map(Cyclist::getIdCyclist);
+    }
 }
