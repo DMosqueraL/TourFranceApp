d09145c8b21169fd0a141584839bd32d490a269d
DMosqueral <dml2410@gmail.com>
2022-07-14 19:56:56 -0500

    [FEAT] Validación de Campos para Cyclist
    Se implementaron las anotaciones que validan los campos del paquete Cyclist

--- src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/collection/Cyclist.java
+++ src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/collection/Cyclist.java
@@ -5,7 +5,11 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 
 @Data
@@ -15,9 +19,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 public class Cyclist {
     @Id
     private String idCyclist;
+    @NotBlank
     private String name;
+    @NotBlank
+    @Indexed(unique = true)
+    @Size(min = 1, max=3)
     private String numCompetitor;
+    @NotBlank
     private String nameTeam;
+    @NotBlank
     private String nationality;
 
 }
--- src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/dto/CyclistDTO.java
+++ src/main/java/co/com/sofka/zonatalentos/tourfranceapp/cyclist/dto/CyclistDTO.java
@@ -2,21 +2,31 @@ package co.com.sofka.zonatalentos.tourfranceapp.cyclist.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.index.Indexed;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @NoArgsConstructor
 @Data
 public class CyclistDTO {
     private String idCyclist;
+    @NotBlank
     private String name;
+    @NotBlank
+    @Indexed(unique = true)
+    @Size(min = 1, max=3)
     private String numCompetitor;
+    @NotBlank
     private String nameTeam;
+    @NotBlank
     private String nationality;
 
-    public CyclistDTO(String idCyclist, String name, String numCompetitor, String nameTeam, String nacionality) {
+    public CyclistDTO(String idCyclist, String name, String numCompetitor, String nameTeam, String nationality) {
         this.idCyclist = idCyclist;
         this.name = name;
         this.numCompetitor = numCompetitor;
         this.nameTeam = nameTeam;
-        this.nationality = nacionality;
+        this.nationality = nationality;
     }
 }
