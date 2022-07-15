79cd2af56d71d67fde59cd2f18f69a5c3de88837
DMosqueral <dml2410@gmail.com>
2022-07-14 21:54:27 -0500

    [FEAT] Router: DeleteTeamRouter
    Se implementó la ruta para eliminar un equipo de la aplicaciÂón

---
+++ src/main/java/co/com/sofka/zonatalentos/tourfranceapp/team/router/DeleteTeamRouter.java
@@ -0,0 +1,29 @@
+package co.com.sofka.zonatalentos.tourfranceapp.team.router;
+
+import co.com.sofka.zonatalentos.tourfranceapp.team.usecases.DeleteTeamUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class DeleteTeamRouter {
+
+    @Bean
+    public RouterFunction<ServerResponse> deleteTeam(DeleteTeamUseCase deleteTeamUseCase){
+        return route(
+                DELETE("/team/delete/{id}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> ServerResponse.accepted()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(deleteTeamUseCase
+                                .apply(request.pathVariable("id")), void.class))
+
+        );
+    }
+}
